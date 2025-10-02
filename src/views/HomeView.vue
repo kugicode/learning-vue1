b62ccebabeab7c016393a6ddbdf39ae1514abf8b
Anas Hersi <101137632+kugicode@users.noreply.github.com>
2025-10-02 23:54:27 +0100

    Following a youtube tutorial to learn vuejs and learning vuejs in university too

---
+++ src/views/HomeView.vue
@@ -0,0 +1,12 @@
+<script setup>
+import Hero from './Hero.vue';
+import HomeCards from './HomeCards.vue';
+import JobListings from './JobListings.vue';
+</script>
+
+<template>
+    <Hero  />
+    <HomeCards />
+    <JobListings :limit="3" :show-button="true"/>
+    
+</template>
